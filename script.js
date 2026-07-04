ae399aaf9decfe2aefbfcdcfa97be1db25e504a1
aniketganorkar03 <aniketganorkar03@gmail.com>
2026-07-04 17:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+
 //your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+const removeBtn = document.getElementById('removeBtn');
+
+removeBtn.addEventListener('click', function() {
+  const selectedIndex = colorSelect.selectedIndex;
+  if (selectedIndex !== -1) {
+    colorSelect.remove(selectedIndex);
+  }
+});
